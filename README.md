71478c3db040a4088f42f5811fe49b892818e1a4
ErikMartensson <ErikMartensson@users.noreply.github.com>
2018-10-30 21:52:31 +0100

    Added relay trigger

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+# Door unlocker
+
 Playground to get my KeyDuino working.
+
+It will look for a predefined hardware ID of a Mifare Ultralight tag and then trigger a relay.
+
+### Hardware:
+* KeyDuino Nano
+* Simple Arduino compatible relay
+
+Copy `config.h.example` to `config.h` and change it to whatever.
--- main.ino
+++ main.ino
@@ -1,53 +1,64 @@
 #include <KeyDuino.h>
+#include <config.h>
 
 KeyDuino keyDuino;
 
 uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
 uint8_t uidLength;
-uint8_t correctTag[] = { 4, 201, 240, 154, 165, 60, 128 };
 
 void setup(void) {
-  Serial.begin(115200);
-
-  keyDuino.begin();
-
-  keyDuino.SAMConfig();
-
-  Serial.println("Waiting for a Mifare Ultralight tag to read.");
+	Serial.begin(115200);
+	keyDuino.begin();
+	keyDuino.SAMConfig();
+	pinMode(PIN_RELAY, OUTPUT);
+	digitalWrite(PIN_RELAY, LOW);
+	Serial.println("Ready");
 }
 
-void blinkLikeHell(void) {
-  for (int i = 0; i < 3; i++) {
-    keyDuino.led(100);
-    delay(100);
-  }
+void blinkFail(void) {
+	for (int i = 0; i < 3; i++) {
+		keyDuino.led(100);
+		delay(100);
+	}
+}
+
+void startUnlock(void) {
+	digitalWrite(PIN_RELAY, HIGH);
+	keyDuino.led(1000);
+}
+
+void stopUnlock(void) {
+	digitalWrite(PIN_RELAY, LOW);
 }
 
 void loop(void) {
-  uint8_t success;
-  success = keyDuino.readTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+	uint8_t success;
+	success = keyDuino.readTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
 
-  if (success) {
-    if (uidLength == 7) {
-      Serial.print("Found chip: ");
-      keyDuino.PrintHex(uid, uidLength);
+	if (success) {
+		if (uidLength == 7) {
+			Serial.print("Found chip: ");
+			keyDuino.PrintHex(uid, uidLength);
 
-      Serial.println("");
-      if (
-        keyDuino.convertUintToString(uid, uidLength) ==
-        keyDuino.convertUintToString(correctTag, uidLength)
-      ) {
-        Serial.println("Chip Found!");
-        blinkLikeHell();
-      } else {
-        Serial.println("Unknown chip");
-      }
-      Serial.println("");
-      delay(500);
+			Serial.println("");
+			if (
+				keyDuino.convertUintToString(uid, uidLength) ==
+				keyDuino.convertUintToString(correctTag, uidLength)
+			) {
+				Serial.println("Chip Found!");
+				startUnlock();
+				stopUnlock();
+			} else {
+				Serial.println("Unknown chip");
+				blinkFail();
+			}
+			Serial.println("");
+			delay(500);
 
-    } else {
-      Serial.println("Detected tag is not Mifare Ultralight.");
-      delay(500);
-    }
-  }
+		} else {
+			Serial.println("Detected tag is not Mifare Ultralight.");
+			blinkFail();
+			delay(500);
+		}
+	}
 }
